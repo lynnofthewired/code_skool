380e50499fee54a9ec74e56d34daec48360a73cc
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-26 23:17:36 -0400

    comments done

--- code_skool-main/2023_spring/lab6part2.py
+++ code_skool-main/2023_spring/lab6part2.py
@@ -2,21 +2,22 @@
 # Monkey Counter
 # This program 
 
-# the point at which the program stops printing and decrementing, and terminates
-# because at 0, there are no more monkeys
+# the constant representing the point at which the program stops printing and decrementing,
+# and terminates, because at 0, there are no monkeys
 NO_MONKEYS = 0
 
 # initialize the variable with the user input 
 monkeys = int( input( "Good day, User! \nHow many monkeys are jumping on this bed?  "))
 
-if monkeys < 0:
-    print(f"\nThere cannot be fewer than 0 monkeys. Please enter a positive ingeter.")
+# error message
+if monkeys < NO_MONKEYS:
+    print(f"\nError: Please enter a positive integer.")
 
-# while loop to decrement the user's integer to zero.
+# while loop to decrement the user's integer to zero
 while monkeys > NO_MONKEYS:
     print(f"\n{monkeys} little monkeys jumping on the bed. \n1 fell off and bumped their head. \nMama called the doctor, and the doctor said, \n\"No more monkeys jumping on the bed!\"")
     monkeys -= 1
 
-# success check 
+# end of program
 if monkeys == 0:
-    print(f"\nNo monkeys jumping on the bed!")
+    print(f"\No monkeys jumping on the bed.")
