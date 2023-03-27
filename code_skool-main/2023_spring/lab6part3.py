927deafeaf860e9b08a7b0a2e69210a9f83a8076
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-26 23:41:22 -0400

    there is no way this works

--- code_skool-main/2023_spring/lab6part3.py
+++ code_skool-main/2023_spring/lab6part3.py
@@ -2,8 +2,20 @@
 # Count to 100 Using Modular Step Size
 # 
 
+# the count initializes at 0
+INIT = 0
+
+# the count finishes at 100
 FINISH = 100
 
-step = int( input("By what number do you wish to count?"))
+# gathers the user-inputted step size
+step = int( input("Please pick a positive integer greater than zero to choose our step size for our counter:  "))
+
+# error message
+if step <= 0:
+    print(f"Error: invalid step size. \nPlease choose any integer greater than zero.")
+
+for i in range ({INIT}, {step}, {FINISH}):
+    print (f {i})
 
 print(f"{step}")
