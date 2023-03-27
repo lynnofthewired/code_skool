4f13de7e601b0e3148dd904d3dfe7e5326ade2f5
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-27 00:02:06 -0400

    asdf

--- code_skool-main/2023_spring/lab6part3.py
+++ code_skool-main/2023_spring/lab6part3.py
@@ -2,18 +2,16 @@
 # Count to 100 Using Modular Step Size
 # 
 
-# the count initializes at 0
-INIT = 0
-
 # the count finishes at 100
 MAX = 100
 
 # gathers the user-inputted step size
 step = int( input("Please pick a positive integer greater than zero to choose our step size for our counter:  "))
 
-start = step + INIT
+# variable is a sum of the initializer starts at the   
+start = step
 
-finish = MAX
+finish = MAX + 1
 
 # error message
 if step <= 0:
