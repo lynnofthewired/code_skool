1c11d5d7f10b43702c0ec0eeb34333868d316865
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-26 22:46:14 -0400

    Formatted grammar

--- code_skool-main/2023_spring/lab6part2.py
+++ code_skool-main/2023_spring/lab6part2.py
@@ -7,7 +7,7 @@ NO_MONKEYS = 0
 monkeys = 8
 
 while monkeys > NO_MONKEYS:
-    print(f"{monkeys} little monkeys jumping on the bed, \n1 fell off and bumped his head, \nmomma called the doctor and the doctor said, \nno more monkeys jumping on the bed.")
+    print(f"{monkeys} little monkeys jumping on the bed. \n1 fell off and bumped their head. \nMama called the doctor, and the doctor said, \n\"no more monkeys jumping on the bed!\"")
     monkeys -= 1
 
 print("Done!")
