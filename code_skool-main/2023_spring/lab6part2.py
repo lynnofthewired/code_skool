a81aa2a6bcacb6182de7d608c45dd2d2aa0dd7e4
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-26 22:53:21 -0400

    added input variable

--- code_skool-main/2023_spring/lab6part2.py
+++ code_skool-main/2023_spring/lab6part2.py
@@ -1,10 +1,11 @@
 # Lynn Makowski
-#
-#
+# Monkey Counter
+# This program 
 
+# the point at which the program stops printing, decrementing
 NO_MONKEYS = 0
 
-monkeys = 8
+monkeys = input( int( "Good day, User! \nHow many monkeys are jumping on this bed?"))
 
 while monkeys > NO_MONKEYS:
     print(f"{monkeys} little monkeys jumping on the bed. \n1 fell off and bumped their head. \nMama called the doctor, and the doctor said, \n\"no more monkeys jumping on the bed!\"")
