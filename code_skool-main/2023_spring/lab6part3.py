ce36db9fee1a6ec0ceef05493619999b21b9c42e
lynnofthewired <s.lynn.makowski@gmail.com>
2023-03-26 23:47:16 -0400

    ?

--- code_skool-main/2023_spring/lab6part3.py
+++ code_skool-main/2023_spring/lab6part3.py
@@ -11,6 +11,8 @@ FINISH = 100
 # gathers the user-inputted step size
 step = int( input("Please pick a positive integer greater than zero to choose our step size for our counter:  "))
 
+start = step + INIT
+
 # error message
 if step <= 0:
     print(f"Error: invalid step size. \nPlease choose any integer greater than zero.")
