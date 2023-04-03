99c125d91314f76f3cb55d199af1ea29bc280387
lynnofthewired <s.lynn.makowski@gmail.com>
2023-04-03 02:17:24 -0400

    budget begin

---
+++ code_skool-main/budget.py
@@ -0,0 +1,16 @@
+# Lynn Makowski
+# Budget Program with Exit Sentinel
+# 
+
+# Variable and prompt to initialize user's monthly budget
+monthly = float( input( f"Good day, user! \nPlease state your monthly budget, and we can begin:  "))
+
+# Sentinel variable
+sentinel = float( input(f"Please report an expense, and if there are none, enter 0:  "))
+
+# Variable for total input expenses
+total = float( input( ))
+
+# 
+while total != 0:
+    total = float( input( "Please report the value of an expense:  "))
