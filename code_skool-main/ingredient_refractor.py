951f3c908107b258db40dc0bf06e257e2152addf
lynnofthewired <s.lynn.makowski@gmail.com>
2023-04-18 19:29:19 -0400

    bullshit

---
+++ code_skool-main/ingredient_refractor.py
@@ -0,0 +1,21 @@
+# A popular website offers a chocolate chip cookie recipe that produces 24 cookies per batch when baked properly. What if a user wanted to bake a differnt amount that 24 cookies? You are to write a Python program that helps the user convert the recipe to make more or less cookies. 
+
+#The sugar cookie recipe requires the following ingredients:
+
+#3 cups of flour
+#0.75 cups of brown sugar
+#2 eggs
+#2 sicks of butter
+#8 ounces of chocolate chips
+#The recipe yeilds 24 cookies per batch.
+
+#Write a program that does the following:
+
+#Asks the user how many cookies they would like to make
+#Converts the amounts needed for each ingredient based on the number of cookies input
+#Hint: Create a variable that is a ratio of desired cookies to original cookie yield and use it as a multiplier on the original recipe ingredient values.
+
+flour = 3
+brown_sugat = 0.75
+eggs = 2
+sticks_butter
